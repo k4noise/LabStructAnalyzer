792aa7053a8ae11c64ae20300cbec0a6f9c4b21e
k4noise <vestertp@gmail.com>
2024-12-14 23:35:17 +0500

    fix(backend): analyzer logic

--- backend/labstructanalyzer/routers/file_router.py
+++ backend/labstructanalyzer/routers/file_router.py
@@ -3,12 +3,10 @@ from starlette.responses import Response
 
 from labstructanalyzer.routers.template_router import template_prefix
 from labstructanalyzer.utils.file_utils import FileUtils
-from labstructanalyzer.utils.rbac_decorator import roles_required
 
 router = APIRouter()
 
 @router.get("/images/temp/{filename}")
-@roles_required(["teacher"])
 async def get_temp_image(filename: str):
     """
     Получить изображение из шаблона.
@@ -16,7 +14,6 @@ async def get_temp_image(filename: str):
     """
     try:
         image = FileUtils.get(template_prefix, filename)
-        FileUtils.remove(template_prefix, filename)
         return Response(content=image, media_type="image/png")
     except IOError as error:
         return HTTPException(404, "Файл не найден")
