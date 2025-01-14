76fbea8258df34bc2138d88364574f48db1236eb
k4noise <vestertp@gmail.com>
2025-01-14 23:42:18 +0500

    feat(frontend): no server sleep ping script

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from "react";
 import { useLoaderData } from "react-router";
 import { UserCourseInfo } from "../../model/user";
+import { api } from "../../utils/sendRequest";
 
 /**
  * Компонент навигационной панели, отображающий информацию о пользователе.
@@ -10,8 +12,22 @@ import { UserCourseInfo } from "../../model/user";
 const Nav = () => {
   const { data: userData } = useLoaderData<{ data: UserCourseInfo }>();
 
+  // удивительной красоты костыль для того, чтобы бэкенд на render.com не ушел в спячку, пока у пользователя запущен фронтенд
+  useEffect(() => {
+    const pingServer = async () => {
+      try {
+        await api.get("/");
+        // действительно, нам тут все равно на возможную ошибку
+      } catch (error) {}
+    };
+
+    const intervalId = setInterval(pingServer, 45000); // 45 сек
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <nav className="min-h-12 flex  items-center justify-end gap-4">
+    <nav className="min-h-12 flex items-center justify-end gap-4">
       {userData?.avatarUrl && (
         <img
           className="w-10 h-10 rounded-full object-cover border-2 border-zinc-400"
