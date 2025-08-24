8b5e14cb18c0e5dce829cb480b6c788f1862ec50
k4noise <vestertp@gmail.com>
2025-08-24 23:20:47 +0500

    refactor(backend): split to external service

--- backend/labstructanalyzer/schemas/modify_template.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional, Sequence
-
-from labstructanalyzer.schemas.template import TemplateDto, TemplateElementUpdateUnit
-
-
-class TemplateToModify(TemplateDto):
-    name: Optional[str] = None
-    max_score: Optional[int] = None
-    elements: Optional[Sequence[TemplateElementUpdateUnit]]
